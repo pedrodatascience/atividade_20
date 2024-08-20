d67a85e6b2c8637d776c0d667de44bbef899fefb
Pedro Henrique Brandão <100478082+pedrodatascience@users.noreply.github.com>
2024-08-20 17:35:18 -0300

    Add files via upload

---
+++ piadas_app.py
@@ -0,0 +1,18 @@
+#importação de bibliotecas
+import pyjokes
+from deep_translator import GoogleTranslator
+
+tradutor = GoogleTranslator(source='auto', target ='pt')
+
+while True:
+    piada = pyjokes.get_joke()
+    piada_traduzida = tradutor.translate(piada)
+    
+    print(piada_traduzida)
+
+    repetir = input('Deseja outra piada?(s/n)  ').lower()
+
+    if repetir == 's':
+        continue
+    else:
+        break
